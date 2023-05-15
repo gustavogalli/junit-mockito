9f3eff20d28f54457fad8278b742794b0452efc2
Gustavo Galli <gustavosgalli@gmail.com>
2023-05-15 19:11:11 -0300

    Tratamento de exceção para objeto não encontrado.

--- src/main/java/com/valdir/api/service/impl/UserServiceImpl.java
+++ src/main/java/com/valdir/api/service/impl/UserServiceImpl.java
@@ -3,6 +3,7 @@ package com.valdir.api.service.impl;
 import com.valdir.api.domain.User;
 import com.valdir.api.repository.UserRepository;
 import com.valdir.api.service.UserService;
+import com.valdir.api.service.exceptions.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -17,6 +18,6 @@ public class UserServiceImpl implements UserService {
     @Override
     public User findById(Integer id) {
         Optional<User> obj = userRepository.findById(id);
-        return obj.orElse(null);
+        return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado!"));
     }
 }

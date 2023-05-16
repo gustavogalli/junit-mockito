acb75176355e323072aa0a6119d6d62c6353b89b
Gustavo Galli <gustavosgalli@gmail.com>
2023-05-16 18:29:38 -0300

    Endpoint findAll User

--- src/main/java/com/valdir/api/service/UserService.java
+++ src/main/java/com/valdir/api/service/UserService.java
@@ -3,8 +3,12 @@ package com.valdir.api.service;
 import com.valdir.api.domain.User;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public interface UserService {
 
     User findById(Integer id);
+
+    List<User> findAll();
 }

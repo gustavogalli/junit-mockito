35e5dcb253758d4c420ffe4c2622d6716369bb21
Gustavo Galli <gustavosgalli@gmail.com>
2023-05-16 18:16:44 -0300

    Retornando DTO para UserResource

--- src/main/java/com/valdir/api/resource/UserResource.java
+++ src/main/java/com/valdir/api/resource/UserResource.java
@@ -1,7 +1,9 @@
 package com.valdir.api.resource;
 
 import com.valdir.api.domain.User;
+import com.valdir.api.domain.dto.UserDTO;
 import com.valdir.api.service.UserService;
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -14,11 +16,14 @@ import org.springframework.web.bind.annotation.RestController;
 public class UserResource {
 
     @Autowired
+    private ModelMapper mapper;
+
+    @Autowired
     private UserService service;
 
     @GetMapping("/{id}")
-    public ResponseEntity<User> findById(@PathVariable Integer id){
-        return ResponseEntity.ok().body(service.findById(id));
+    public ResponseEntity<UserDTO> findById(@PathVariable Integer id){
+        return ResponseEntity.ok().body(mapper.map(service.findById(id), UserDTO.class));
     }
 
 }

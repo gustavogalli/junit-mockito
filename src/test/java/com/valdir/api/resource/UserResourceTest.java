e626438cf218e5864d445a59bbbff2c8b355c8b1
Gustavo Galli <gustavosgalli@gmail.com>
2023-05-19 12:20:35 -0300

    Teste para endpoint findAll da classe UserResource implementado

--- src/test/java/com/valdir/api/resource/UserResourceTest.java
+++ src/test/java/com/valdir/api/resource/UserResourceTest.java
@@ -3,6 +3,8 @@ package com.valdir.api.resource;
 import com.valdir.api.domain.User;
 import com.valdir.api.domain.dto.UserDTO;
 import com.valdir.api.service.UserService;
+import com.valdir.api.service.exceptions.DataIntegrityViolationException;
+import com.valdir.api.service.exceptions.ObjectNotFoundException;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
@@ -11,8 +13,11 @@ import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
 import org.modelmapper.ModelMapper;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -67,7 +72,38 @@ class UserResourceTest {
     }
 
     @Test
+    void whenFindByIdReturnsException(){
+        when(service.findById(any())).thenThrow(new ObjectNotFoundException("Objeto não encontrado!"));
+
+        try{
+            resource.findById(ID);
+        } catch(Exception exception){
+            assertEquals(ObjectNotFoundException.class, exception.getClass());
+            assertEquals("Objeto não encontrado!", exception.getMessage());
+        }
+
+    }
+
+    @Test
     void findAll() {
+        when(service.findAll()).thenReturn(List.of(user, user));
+        when(mapper.map(any(), any())).thenReturn(userDTO);
+
+        ResponseEntity<List<UserDTO>> response = resource.findAll();
+
+        assertNotNull(response);
+        assertNotNull(response.getBody());
+        assertEquals(ResponseEntity.class, response.getClass());
+        assertEquals(ArrayList.class, response.getBody().getClass());
+        assertEquals(UserDTO.class, response.getBody().get(0).getClass());
+        assertEquals(HttpStatus.OK, response.getStatusCode());
+        assertEquals(2, response.getBody().size());
+
+        assertEquals(ID, response.getBody().get(0).getId());
+        assertEquals(NAME, response.getBody().get(0).getName());
+        assertEquals(MAIL, response.getBody().get(0).getEmail());
+        assertEquals(PASSWORD, response.getBody().get(0).getPassword());
+
     }
 
     @Test
